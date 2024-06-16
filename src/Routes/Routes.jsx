e8d84d87e1fac41d52ff34d4021e0c38625a5703
KHAN FAYSAL <khanfaysal@KHANs-MacBook-Pro.local>
2024-06-16 13:02:41 +0600

    add some data

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
         children:[
             {
                 path:"/",
-                element:<Home></Home>
+                element:<Home></Home>,
             }
         ]
     }
